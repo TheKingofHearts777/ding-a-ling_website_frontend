355eace791eb96bda168b52f1d8d45a5ce12113a
Dylan Brodie <brodied@upwlatt.edu>
2026-05-21 13:59:33 -0500

    revert "added app.css as import to app.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from './About.jsx';
 import ProductRequest from './ProductRequest.jsx';
 import Portfolio from './Portfolio.jsx';
 import Home from './Home.jsx';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 
