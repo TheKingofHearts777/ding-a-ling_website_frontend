af6d9c7e4516c7242c7ffc963fb3bef9eae792f4
Dylan Brodie <brodied@upwlatt.edu>
2026-05-21 13:25:28 -0500

    updated color scheme template and general layout of website

--- src/About.jsx
+++ src/About.jsx
@@ -5,18 +5,18 @@ const About = () => {
         <div>
             <h1>About</h1>
             <div>
-            <ul>
-                <li>Blacksmithing demos</li>
-                <li>Blacksmith training</li>
-                <li>
-                    Blacksmithing to sell
-                    <ul>
-                        <li>Custom orders</li>
-                        <li>Pre-made items</li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+                <ul>
+                    <li>Blacksmithing demos</li>
+                    <li>Blacksmith training</li>
+                    <li>
+                        Blacksmithing to sell
+                        <ul>
+                            <li>Custom orders</li>
+                            <li>Pre-made items</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
